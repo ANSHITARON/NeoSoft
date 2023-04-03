f9f8ea4ffcc3f82847eef9b17c28ed5af0778f2c
ANSHITARON <aronanshit@gmail.com>
2023-04-04 01:17:46 +0530

    securityhead base

--- src/main/java/com/security/demo/config/SecurityConfig.java
+++ src/main/java/com/security/demo/config/SecurityConfig.java
@@ -4,31 +4,50 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-        @Autowired
+    @Autowired
     private MongoUserDetailsService MongouserDetailsService;
-   // @Bean
-   // public UserDetailsService userDetailsService(){
+    // @Bean
+    // public UserDetailsService userDetailsService(){
     //   UserDetails normal= User.builder().username("ankit").password(passwordEncoder().encode("ankit")).roles("NORMAL").build();
 
-     // UserDetails admin= User.builder().username("anshit").password(passwordEncoder().encode("anshit")).roles("ADMIN").build();
+    // UserDetails admin= User.builder().username("anshit").password(passwordEncoder().encode("anshit")).roles("ADMIN").build();
 
 
     //   return new InMemoryUserDetailsManager(normal, admin);
     @Bean
-    public DaoAuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider daoAuthenticationProvider =new DaoAuthenticationProvider();
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
         daoAuthenticationProvider.setUserDetailsService(this.MongouserDetailsService);
-       daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
         return daoAuthenticationProvider;
     }
+
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    /*@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    {
+        http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("login.html").loginProcessingUrl("/process-url")
+                .defaultSuccessUrl("/dashboard").failureUrl("error")
+                .and().logout().logoutUrl("/logout");
+        return http.build();
+    }
+
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().disable().authorizeRequests()
+                .anyRequest().authenticated().and().httpBasic();
+        return http.build();
+    }
 }
